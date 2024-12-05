627573d5c9e83b27f156990c800e2d8f9e5ce2f3
Ray <nikanop9@yandex.ru>
2024-12-05 21:24:39 +0700

    bdissue

--- sql-scripts/init.sql
+++ sql-scripts/init.sql
@@ -4,3 +4,13 @@ CREATE TABLE `users` (
   `email` VARCHAR(255) UNIQUE NOT NULL,
   `password` VARCHAR(255) NOT NULL,
 );
+CREATE TABLE `booking` (
+  `date` date NOT NULL,
+  `time` time NOT NULL,
+  `type_court` varchar(20) NOT NULL,
+  `num_court` int(11) NOT NULL,
+  `price` int(11) NOT NULL,
+  `type_pay` varchar(20) NOT NULL,
+  `user_id` bigint(20) UNSIGNED NOT NULL,
+  `id` int(11) UNSIGNED NOT NULL
+)
